154e6af32ee10915e7347b36176e9401b5d1e07a
kanmitcode <oyekanmi.owolabi@mtn.com>
2025-01-11 15:38:44 +0100

    modified code

--- src/bot.ts
+++ src/bot.ts
@@ -22,8 +22,7 @@ const clientOptions: ClientOptions = {
   },
   webVersionCache: {
     type: "remote",
-    remotePath:
-      "https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html",
+    remotePath: "https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html",
   },
 };
 
