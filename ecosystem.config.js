94e3795cd1bd3f825e3052de382eb6f5e603667d
kanmitcode <135128220+kanmitcode@users.noreply.github.com>
2025-01-12 12:49:00 +0100

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,19 +8,19 @@ module.exports = {
             "PORT": 3000,
             "NODE_ENV": "dev",
             "DATABASE_URL": "postgresql://kanmit:Manager2025@localhost:5432/supportbotdb?schema=public",
-            "OPENAI_API_KEY": "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"
+            "OPENAI_API_KEY": "enter-your-open-ai-key"
           },
           env_test: {
             "PORT": 3000,
             "NODE_ENV": "test",
             "DATABASE_URL": "postgresql://kanmit:Manager2025@localhost:5432/supportbotdb?schema=public",
-            "OPENAI_API_KEY": "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"
+            "OPENAI_API_KEY": "enter-your-open-ai-key"
           },
           env_production: {
             "PORT": 3000,
             "NODE_ENV": "production",
             "DATABASE_URL": "postgresql://kanmit:Manager2025@localhost:5432/supportbotdb?schema=public",
-            "OPENAI_API_KEY": "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"
+            "OPENAI_API_KEY": "enter-your-open-ai-key"
           },
           watch: 'false',
           ignore_watch : [ "*.log"],
